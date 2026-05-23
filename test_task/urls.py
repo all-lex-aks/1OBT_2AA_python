7d834457100fd2b6455cdee982fa638e20342b91
all-lex-aks <alexei.k.aksyutin@gmail.com>
2026-05-23 11:56:07 +0300

    Add new requirements.txt and .gitignore

--- test_task/urls.py
+++ test_task/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-rom django.conf import settings
+from django.conf import settings
 from django.conf.urls import url
 from django.shortcuts import render
 
